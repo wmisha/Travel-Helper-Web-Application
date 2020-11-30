47a39c072c691b14c5e7d332577d64d4cce1ed41
Zhenzhen Wang <zwang211@dons.usfca.edu>
2020-11-30 00:08:11 -0800

    working on frontend, index.html, learn bootstrap 4: containers, Images, Buttons, Navs, Navbar, Forms.

--- src/main/java/BackEndServer.java
+++ src/main/java/BackEndServer.java
@@ -23,13 +23,20 @@ public class BackEndServer {
         context1.addServlet(UserLoginServlet.class, "/login");
         context1.addServlet(WelcomeServlet.class, "/welcome");
 
-        ;
+
+        // initialize Velocity
+        VelocityEngine velocity = new VelocityEngine();
+        velocity.init();
+
+        // set velocity as an attribute of the context so that we can access it
+        // from servlets
+        context1.setContextPath("/");
+        context1.setAttribute("templateEngine", velocity);
 
         ServletContextHandler context2 = new ServletContextHandler();
         context2.setContextPath("/hotel");
         context2.addServlet(SearchHotelServlet.class, "/");
         context2.addServlet(SearchReviewServlet.class, "/review");
-
 
 
         HandlerList handlers = new HandlerList();
