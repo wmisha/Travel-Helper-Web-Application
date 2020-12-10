649b382df31e8edb82cff9845fd4fd7eddf2ba62
Zhenzhen Wang <zwang211@dons.usfca.edu>
2020-12-09 17:09:07 -0800

    working on mySql tables.

---
+++ src/main/java/server/LoadJsonToTables.java
@@ -0,0 +1,99 @@
+package server;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import hotelapp.Hotel;
+import hotelapp.Review;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LoadJsonToTables {
+
+    protected static final DatabaseHandler dbHandler = DatabaseHandler.getInstance();
+    private Gson gson;
+    private String hotelsFile;
+    private String reviewsDir;
+
+    public LoadJsonToTables(String hotelsFile, String reviewsDir) {
+        this.gson = new Gson();
+        this.hotelsFile = hotelsFile;
+        this.reviewsDir = reviewsDir;
+    }
+
+    public  void parseHotels() {
+
+        try (FileReader br = new FileReader(hotelsFile)) {
+
+            String fileData = new String(Files.readAllBytes(Paths.get(hotelsFile)));
+            JsonParser parser = new JsonParser();
+            JsonObject jo = (JsonObject) parser.parse(fileData);
+
+            JsonArray jsonArr = jo.getAsJsonArray("sr"); // array of Json
+
+            Hotel[] hotels = gson.fromJson(jsonArr, Hotel[].class);
+            for (Hotel h : hotels) {
+                dbHandler.insertValueToHotels(h.getId(),h.getF(),h.getFullAddress(),h.getLink());
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read the file: " + e);
+        }
+        System.out.println("finish parsing hotel");
+    }
+   public Path getPath(){
+        return Paths.get(reviewsDir);
+   }
+    public void traverseReviews(Path path) {
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    traverseReviews(entry);
+                } else {
+                    parseReview(entry.toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("finish parsing review............");
+    }
+    public void parseReview(String filePath) {
+        Review[] reviews;
+
+        try (FileReader br = new FileReader(filePath)) {
+
+            String fileData = new String(Files.readAllBytes(Paths.get(filePath)));
+            JsonParser parser = new JsonParser();
+            JsonObject jo = (JsonObject) parser.parse(fileData);
+            JsonArray jsonArr = jo.getAsJsonObject("reviewDetails")
+                    .getAsJsonObject("reviewCollection")
+                    .getAsJsonArray("review");
+
+            reviews = gson.fromJson(jsonArr, Review[].class);
+            for (Review r : reviews) {
+                 dbHandler.insertValuesToReviews(r.getReviewId(), r.getHotelId(), r.getRatingOverall(),
+                        r.getTitle(), r.getReviewText(), r.getUserNickname(), r.getDate(), r.getUserId());
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read the file: " + e);
+        }
+
+    }
+    public static void main(String[] args){
+        LoadJsonToTables loadJsonToTables = new LoadJsonToTables("input/hotels.json","input/reviews");
+        Path path = loadJsonToTables.getPath();
+        loadJsonToTables.parseHotels();
+        loadJsonToTables.traverseReviews(path);
+
+
+    }
+
+}
+
